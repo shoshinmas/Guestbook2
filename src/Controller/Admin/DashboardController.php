8a60ce7ac697a76db530a902648a5063776680fd
Marcin Stolarz <shoshinmas@protonmail.com>
2021-06-03 19:50:13 +0200

    Added CRUD, basic Admin functions

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,8 +5,11 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Comment;
+use App\Entity\Conference;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -15,7 +18,10 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -26,7 +32,8 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+                yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
+                yield MenuItem::linkToCrud('Conferences', 'fas fa-map-marker-alt', Conference::class);
+                yield MenuItem::linkToCrud('Comments', 'fas fa-comments', Comment::class);
     }
 }
